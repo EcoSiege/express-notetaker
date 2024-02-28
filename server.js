b102d8ee2fdfa59ac6dd71d48e4de535b1eb6456
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2024-02-28 10:20:22 -0600

    added get routes

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const path = require('path');
+const db = require('./db/db.json');
 
 const PORT = 3001;
-const app =express();
+const app = express();
+
+app.use(express.static('public'));
 
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
